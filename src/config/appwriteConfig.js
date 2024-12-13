eb0eb7a44bac769edc2c233e52ca4101994ccebc
Shreyas M <shreyas.m246418@gmail.com>
2024-12-13 11:12:29 +0530

    using appwrite 19 new acc

--- src/config/appwriteConfig.js
+++ src/config/appwriteConfig.js
@@ -1,8 +1,13 @@
 import { Client, Account } from 'appwrite';
 
-const client = new Client();
+/*const client = new Client();
 client.setEndpoint('https://cloud.appwrite.io/v1') 
-      .setProject('675a849c002df03af2d5');
+      .setProject('675a849c002df03af2d5');*/
+
+const client = new Client();
+client.setProject('675bc7ba0009962686c7');
+client.setEndpoint('https://cloud.appwrite.io/v1')
+
 
 const account = new Account(client);
 
